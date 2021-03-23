245ae4fc0ef78e98a4c0921f99560125771b8357
Anders Lundsgård <anders@utvecklare.eu>
2021-03-23 21:27:35 +0100

    Tiny start of configure story

--- python_files/src/awstoken.py
+++ python_files/src/awstoken.py
@@ -7,7 +7,16 @@ import click
 @click.option('--long-term-cred-profile', '-l', required='true', type=click.STRING, help='The name of the AWS profile in ~/.aws/credentials where long term credentials are')
 @click.option('--account-id', '-i', type=click.STRING, help='Bypass the Organization accounts check with providing the account id directly')
 @click.option('--profile-to-configure', '-p', default='default', type=click.STRING, help='The profile to be configured. Default is \'default\' profile')
+@click.option('--configure', '-c', is_flag='true', help='Initiate a configuration of an aws-mfa-token profile')
 def main(**kwargs):
+    configure = kwargs['configure']
+    if configure:
+        print('Ready to configure!!')
+        long_term_cred_profile = input('./aws/config profile with long term access keys: ')
+        account_id = input('AWS Account ID: ')
+
+        return
+
     role_name = kwargs['role_name']
     long_term_cred_profile = kwargs['long_term_cred_profile']
     account_id = kwargs['account_id']
